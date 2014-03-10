cb15fa121ac6be564fb34912735bb1849bbfda6c
happyt <ian.happytuesdays@gmail.com>
2014-03-10 22:24:58 +0000

    Async version

--- pist.js
+++ pist.js
@@ -3,7 +3,9 @@
  * Created by Mortoni on 06/03/14.
  */
 var FeedParser = require('feedparser')
-    , request = require('request');
+    , request = require('request')
+    , http = require('http')
+    , async = require('async');
 
 var db = require("./dblogger");
 var udp = require('udp');
@@ -15,6 +17,88 @@ var VizPort = 6101;
 
 var feedURL = "http://feed.scribblelive.com/rss.aspx?Id=470798&Token=cc310fdb-4bca-464a-b919-10ffa0589171&c=1670";
 
+feedURL = "http://singletrackworld.com/forum/rss/";
+
+feedURL = "http://feeds.bbci.co.uk/news/politics/rss.xml";
+
+var feedMeta;
+var episodes = [];
+var running = true;
+
+function processMessage(item, doneCallback)
+{
+ //   console.log(pubDate.getHours() + ":" + pubDate.getMinutes() + ": " + title);
+    console.log(item.title);
+    return doneCallback(null);
+}
+
+ // async.forever {
+
+
+var getNextBlock = function(done){
+    http.get(feedURL, function(res) {
+        res.pipe(new FeedParser({}))
+            .on('error', function(error){
+                // TODO: Tell the user we just had a melt-down
+                console.log(error);
+            })
+
+            .on('meta', function(meta){
+                // Store the metadata for later use
+                feedMeta = meta;
+            })
+
+            .on('readable', function(){
+                var stream = this, item;
+
+                while (item = stream.read()){
+                    // Each 'readable' event will contain 1 article
+                    // Add the article to the list of episodes
+                    var ep = {
+                        'title': item.title,
+                        'mediaUrl': item.link,
+                        'pubDate': item.pubDate
+                    };
+                    episodes.push(ep);
+                }
+
+                async.map(episodes, processMessage, function(err){
+                    console.log("\n\nFinished!");
+                });
+            })
+
+            .on('end', function(){
+                console.log("\n\n\n\nEnd of feed block....");
+                /*
+                var result = {
+                    'feedName': feedMeta.title,
+                    'feedArtist': feedMeta['itunes:author']['#'],
+                    'website': feedMeta.link,
+                    'albumArt': {
+                        'url': feedMeta.image.url,
+                        'width': parseInt(feedMeta['rss:image']['width']['#']),
+                        'height': parseInt(feedMeta['rss:image']['height']['#'])
+                    },
+                    'episodes': episodes
+                };
+
+            */
+
+        });
+    });
+    setImmediate(done);
+ }
+
+async.forever(getNextBlock, function(err){
+    console.log("Block....!");
+});
+
+
+
+
+
+
+/*
 var req = request(feedURL)
     , feedparser = new FeedParser();
 
@@ -49,12 +133,12 @@ feedparser.on('readable', function() {
 
     }
     var message = new Buffer('0 RENDERER*MAP SET_STRING_ELEMENT pist Alphabetti2 and Fours\0');
-
+*/
 //    client.send(message, 0, message.length, VizPort, VizIP, function(err, bytes) {
 //        console.log(err);
 //        client.close();
 //    });
-});
+
 /**
  * Created by Sony laptop on 07/03/14.
  */
